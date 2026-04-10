2facc993bcb87343759f487d526b4e67160d8d10
miral-xliv <miralpanchal2005@gmail.com>
2026-04-11 01:13:13 +0530

    Update matcher.py

--- engine/matcher.py
+++ engine/matcher.py
@@ -8,15 +8,9 @@ def load_model():
 model = load_model()
 
 def calculate_ats_score(data, jd_input):
-    """
-    Calculates semantic similarity and returns both Match and Gap scores.
-    """
     resume_text = str(data)
-    
     embeddings = model.encode([resume_text, jd_input])
     cosine_sim = util.cos_sim(embeddings[0], embeddings[1])
-    
     score = round(float(cosine_sim) * 100, 2)
     gap_score = round(100 - score, 2)
-    
     return score, gap_score
